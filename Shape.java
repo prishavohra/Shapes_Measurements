a65d49f892dee04a937da09da58ebb07ba520e85
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 01:54:46 +0530

    Initialize Shape.java

--- Shape.java
+++ Shape.java
@@ -1,1 +1,8 @@
+// Shape.java
 
+abstract class Shape {
+    protected double dim_one;
+    protected double dim_two;
+    protected double dim_three;
+    protected int num_sides;
+    protected String shapeName;  // New field to store the shape name
