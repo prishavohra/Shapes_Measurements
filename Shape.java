3417c2613eba85201bb3698320f3079e2ac1eb7c
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 01:55:26 +0530

    Zero parameter constructor

--- Shape.java
+++ Shape.java
@@ -6,3 +6,11 @@ abstract class Shape {
     protected double dim_three;
     protected int num_sides;
     protected String shapeName;  // New field to store the shape name
+
+    // Zero Parameter Constructor
+    public Shape(){
+        dim_one = dim_two = dim_three = 0;
+        num_sides = 0;
+        shapeName = "";
+    }
+    
