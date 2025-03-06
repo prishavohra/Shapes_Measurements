9ac1e8417189954bc17e4b6d214a8e5f980f8415
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:00:32 +0530

    Functions to calculate area, perimeter, volume

--- Rectangle.java
+++ Rectangle.java
@@ -6,3 +6,19 @@ class Rectangle extends Shape {
     public Rectangle(double length, double width) {
         super(length, width, 0, 4, "Rectangle");
     }
+    
+    public double calculateArea(){
+        return dim_one * dim_two;
+    }
+    
+    // Calculate the perimeter of the rectangle
+    public double calculatePerimeter(){
+        return 2 * (dim_one + dim_two);
+    }
+    
+    // Rectangle is 2D so volume is 0
+    public double calculateVolume() {
+        return 0.0;
+    }
+}
+
