d4491858f2b68ec9835586c4b1c1551a0414cc15
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:11:28 +0530

    Constructor to accept radius, height of cylinder

--- Cylinder.java
+++ Cylinder.java
@@ -1,1 +1,13 @@
+// Cylinder.java
+// Extends Shape and implements Volume for 3D cylinder calculations
 
+public class Cylinder extends Shape implements Volume {
+    private double radius;
+    private double height;
+    
+    // Constructor accepting the radius and height of the cylinder
+    public Cylinder(double radius, double height) {
+        super(radius, height, 0, 0, "Cylinder"); // Number of sides is not applicable; set to 0
+        this.radius = radius;
+        this.height = height;
+    }
