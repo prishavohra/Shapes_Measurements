f54aa1a3591961db108a79a8017b46631652311c
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:19:32 +0530

    Switch case for Menu Operations

--- MAIN.java
+++ MAIN.java
@@ -8,3 +8,49 @@ class Main {
         Scanner scan = new Scanner(System.in);
         int choice;
         
+        do {
+            System.out.println("\n------ Shape Calculator Menu ------");
+            System.out.println("1. Rectangle");
+            System.out.println("2. Circle");
+            System.out.println("3. Cuboid");
+            System.out.println("4. Square");
+            System.out.println("5. Sphere");
+            System.out.println("6. Cylinder");
+            System.out.println("7. Equilateral Pyramid");
+            System.out.println("8. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scan.nextInt();
+            
+            switch(choice) {
+                case 1:
+                    processRectangle(scan);
+                    break;
+                case 2:
+                    processCircle(scan);
+                    break;
+                case 3:
+                    processCuboid(scan);
+                    break;
+                case 4:
+                    processSquare(scan);
+                    break;
+                case 5:
+                    processSphere(scan);
+                    break;
+                case 6:
+                    processCylinder(scan);
+                    break;
+                case 7:
+                    processEquilateralPyramid(scan);
+                    break;
+                case 8:
+                    System.out.println("Exiting the program.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
+            }
+        } while(choice != 8);
+        
+        scan.close();
+    }
+    
