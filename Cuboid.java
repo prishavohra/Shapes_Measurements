8a25fd14d388c1ca8518c4424ee8a21560e7fcd1
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:04:03 +0530

    Functions to calculate area, perimeter, volume

--- Cuboid.java
+++ Cuboid.java
@@ -8,3 +8,16 @@ class Cuboid extends Shape implements Volume {
     public Cuboid(double length, double width, double height) {
         super(length, width, height, 6, "Cuboid");
     }
+
+    public double calculateArea(){
+        return 2 * ((dim_one * dim_two) + (dim_one * dim_three) + (dim_two * dim_three));
+    }
+    
+    public double calculatePerimeter(){
+        return 4 * (dim_one + dim_two + dim_three);
+    }
+
+    public double calculateVolume(){
+        return dim_one * dim_two * dim_three;
+    }
+}
