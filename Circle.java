9ece2503006b1055bef23ef770538067215d51f6
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:01:56 +0530

    Constructor accepting radius

--- Circle.java
+++ Circle.java
@@ -1,1 +1,8 @@
+// Circle.java
+// Polymorphism implementation for Circle
 
+class Circle extends Shape {
+    // Constructor accepting the radius
+    public Circle(double radius) {
+        super(radius, 0, 0, 1, "Circle");
+    }
