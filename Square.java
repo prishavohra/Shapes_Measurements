28dc6ce612e72a98ad82713af69d9c01f765b22e
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:05:36 +0530

    Extending square class from rectangle

--- Square.java
+++ Square.java
@@ -1,1 +1,10 @@
+// Square.java
+// Extends Rectangle to implement Square shape calculations
 
+public class Square extends Rectangle {
+    // Constructor accepting the side length; a square is a rectangle with equal sides.
+    public Square(double side) {
+        super(side, side);
+        // Override the shape name to "Square"
+        this.shapeName = "Square";
+    }
