8c581ca08eae52712cc427131fe16af2be85965a
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:08:44 +0530

    Accepting radius of sphere

--- Sphere.java
+++ Sphere.java
@@ -1,1 +1,11 @@
+// Sphere.java
+// Extends Shape and implements Volume for 3D sphere calculations
 
+public class Sphere extends Shape implements Volume {
+    private double radius;
+    
+    // Constructor accepting the radius
+    public Sphere(double radius) {
+        super(radius, 0, 0, 0, "Sphere"); // For sphere, number of sides is not applicable (set to 0)
+        this.radius = radius;
+    }
