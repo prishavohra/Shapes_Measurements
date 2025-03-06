7a4b4c657aff0de82756d386602cfb2616b93033
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:14:20 +0530

    Accepting base length and height of pyramid

--- EquilateralPyramid.java
+++ EquilateralPyramid.java
@@ -1,1 +1,13 @@
+// EquilateralPyramid.java
+// Extends Shape and implements Volume for 3D equilateral pyramid (square base) calculations
 
+public class EquilateralPyramid extends Shape implements Volume {
+    private double base;
+    private double height;
+    
+    // Constructor accepting the base side length and height of the pyramid
+    public EquilateralPyramid(double base, double height) {
+        super(base, height, 0, 0, "Equilateral Pyramid"); // For pyramid, number of sides is not directly used
+        this.base = base;
+        this.height = height;
+    }
