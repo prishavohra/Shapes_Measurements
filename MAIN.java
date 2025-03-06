911c26f01a20134276aa41f91d98116cdfa09f4d
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:18:50 +0530

    Intialize class Main

--- MAIN.java
+++ MAIN.java
@@ -1,1 +1,10 @@
+// Main.java
+// Menu-driven program to calculate area, perimeter, and volume of selected shapes.
 
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        
