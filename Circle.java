3d9b5b4ca7c05bd0b3661809b5d6af271f409bdb
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 02:02:14 +0530

    Functions to calculate area, perimeter, volume

--- Circle.java
+++ Circle.java
@@ -6,3 +6,16 @@ class Circle extends Shape {
     public Circle(double radius) {
         super(radius, 0, 0, 1, "Circle");
     }
+    
+    public double calculateArea(){
+        return Math.PI * Math.pow(dim_one, 2);
+    }
+    
+    public double calculatePerimeter(){
+        return 2 * Math.PI * dim_one;
+    }
+    
+    public double calculateVolume() {
+        return 0.0;
+    }
+}
