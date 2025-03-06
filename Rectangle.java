d8d86226531bf990f5b6bd31e0c3c0137d13b26f
prishavohra <152265967+prishavohra@users.noreply.github.com>
2025-03-07 01:59:47 +0530

    Constuctor accepting length and width

--- Rectangle.java
+++ Rectangle.java
@@ -1,1 +1,8 @@
+// Rectangle.java
+// Polymorphism implementation for Rectangle
 
+class Rectangle extends Shape {
+    // Constructor accepting length and width
+    public Rectangle(double length, double width) {
+        super(length, width, 0, 4, "Rectangle");
+    }
